9a22bbe40fba88a102bbd283ecb9e8cac51bcb74
Dominik Sieber <dominik.s1997@gmx.de>
2023-12-02 10:21:48 +0100

    Upload Day 1

---
+++ Day1.py
@@ -0,0 +1,27 @@
+def main():
+    with open('Puzze_Input_Day_1') as file:
+        puzzle_input = file.readlines()
+
+    word_to_digit = {'one': 'o1e', 'two': 't2o', 'three': 't3e', 'four': 'f4r', 'five': 'f5e', 'six': 's6x',
+                     'seven': 's7n', 'eight': 'e8t', 'nine': 'n9e'}
+
+    def digits(line):
+        digit = [integer for integer in line if integer.isdigit()]
+        if digit:
+            return int(digit[0]+digit[-1])
+        return 0
+
+    def convert_to_digit(input_text):
+        for word, digit in word_to_digit.items():
+            input_text = input_text.replace(word, digit)
+        return input_text
+
+    value = sum(digits(line) for line in puzzle_input)
+    print(value)
+    converted_text = [convert_to_digit(line) for line in puzzle_input]
+    value_2 = sum(digits(line) for line in converted_text)
+    print(value_2)
+
+
+if __name__ == '__main__':
+    main()
